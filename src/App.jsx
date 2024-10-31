61f97e5fe9a58a566eb83aa7860c9b5844f72d48
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-10-31 23:13:57 +0530

    clear default data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/header';
+import UserTag from './components/userdata';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function App(){
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header />
+    <h1>Thisal Karunarathna</h1>
+    <UserTag name="thisal" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4FZRP5ugy_IOfnRRkOiIBV55SMYMEmX592gtArK73r5kYe9UjnChqPhrJm86Iz_hjJbs&usqp=CAU"/>
     </>
   )
 }
 
-export default App
+export default App;
