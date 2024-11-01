bd1e7ce918521e78982d9cbf2aaa762e4cf870b2
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-11-01 23:32:21 +0530

    install tailwind and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import './App.css';
-import Header from './components/header';
-import UserTag from './components/userdata';
-
-function App(){
-  return(
-    <>
-    <Header />
-    <h1>Thisal Karunarathna</h1>
-    <UserTag name="thisal" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4FZRP5ugy_IOfnRRkOiIBV55SMYMEmX592gtArK73r5kYe9UjnChqPhrJm86Iz_hjJbs&usqp=CAU"/>
-    </>
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold bg-slate-600 underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App;
