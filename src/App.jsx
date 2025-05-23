23ee4f1e4ceded8f7ead8d9382227e73608f2924
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-05-23 10:59:31 +0530

    data retrieve from array maping

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Home from "../src/pages/client/home.jsx"
+import Home from "../src/pages/client/home.jsx";
 import Dashboard from "./pages/admin/dashboard.jsx";
 
 function App() {
   return (
-  <BrowserRouter>
-  <Routes path="/">
-  <Route path="/admin/*" element={<Dashboard />} />
-  <Route path="/*" element={<Home />} />
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes path="/">
+        <Route path="/admin/*" element={<Dashboard />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
