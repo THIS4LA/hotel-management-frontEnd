ead5286e877e93e3423afc563ef6c89719ac63de
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-06-05 20:32:50 +0530

    add routes for client home and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../src/pages/client/home.jsx";
 import Dashboard from "./pages/admin/dashboard.jsx";
+import Login from "./pages/authentication/login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes path="/">
+        <Route path="/client/*" element={<Home />} />
         <Route path="/admin/*" element={<Dashboard />} />
-        <Route path="/*" element={<Home />} />
+        <Route path="/*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
