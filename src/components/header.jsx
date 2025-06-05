e1d66decdd62194f78a2c7b908c56fe20335f556
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-06-05 20:33:09 +0530

    clear props

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header (){
     return(
         <div className="w-full px-5 py-2 h-[64px] bg-red-400 flex justify-between items-center">
             <h1 className="text-[16px] font-semibold text-white">Hotel Management System</h1>
-            <UserData name="thisal karunarathna"/>
+            <UserData/>
         </div>
     )
 }
