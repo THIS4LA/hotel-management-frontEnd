33a24bf92fbad1d15cdad183382f034b429b0a96
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-06-05 20:34:39 +0530

    token decode func by axios

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -1,12 +1,32 @@
-import profilePic from "./../assets/IMG_2436.jpg"
+import axios from "axios";
+import profilePic from "./../assets/IMG_2436.jpg";
+import { useState } from "react";
 
-function UserData(props){
-    return(
-        <div className="flex items-center space-x-2 cursor-pointer">
-            <h1 className="text-[12px] text-white">{props.name}</h1>
-            <img className="w-[35px] h-[35px] rounded-full" src={profilePic}/>
-        </div>
-    )
+function UserData() {
+  const [name, setName] = useState("");
+
+  const token = localStorage.getItem("token");
+  if (token != null) {
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + "/api/users/", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/type",
+        },
+      })
+      .then((res) => {
+        const firstName = res.data.user.firstName;
+        const lastName = res.data.user.lastName;
+        const name = firstName +" "+ lastName;
+        setName(name);
+      });
+  }
+  return (
+    <div className="flex items-center space-x-2 cursor-pointer">
+      <h1 className="text-[12px] text-white">{name}</h1>
+      <img className="w-[35px] h-[35px] rounded-full object-cover" src={profilePic} alt="profilePic" />
+    </div>
+  );
 }
 
 export default UserData;
