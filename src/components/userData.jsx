18585c2f873718a71af1714d8e8f4cf32618de5f
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-06-05 20:57:52 +0530

    use useEffect for avoid repeating get token info func and add logout btn

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -1,30 +1,53 @@
 import axios from "axios";
 import profilePic from "./../assets/IMG_2436.jpg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UserData() {
   const [name, setName] = useState("");
+  const [userFound, setUserFound] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token != null) {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/users/", {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/type",
+          },
+        })
+        .then((res) => {
+          const firstName = res.data.user.firstName;
+          const lastName = res.data.user.lastName;
+          const name = firstName + " " + lastName;
+          setName(name);
+          setUserFound(true);
+        });
+    } else {
+      setName(" ");
+    }
+  }, [userFound]);
 
   const token = localStorage.getItem("token");
-  if (token != null) {
-    axios
-      .get(import.meta.env.VITE_BACKEND_URL + "/api/users/", {
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/type",
-        },
-      })
-      .then((res) => {
-        const firstName = res.data.user.firstName;
-        const lastName = res.data.user.lastName;
-        const name = firstName +" "+ lastName;
-        setName(name);
-      });
-  }
   return (
     <div className="flex items-center space-x-2 cursor-pointer">
       <h1 className="text-[12px] text-white">{name}</h1>
-      <img className="w-[35px] h-[35px] rounded-full object-cover" src={profilePic} alt="profilePic" />
+      <img
+        className="w-[35px] h-[35px] rounded-full object-cover"
+        src={profilePic}
+        alt="profilePic"
+      />
+      <button
+        onClick={() => {
+          localStorage.removeItem("token"),
+            setUserFound(false),
+            navigate("/login");
+        }}
+        className="bg-white text-red-500 text-sm px-3 py-1 rounded hover:bg-red-100"
+      >
+        Logout
+      </button>
     </div>
   );
 }
