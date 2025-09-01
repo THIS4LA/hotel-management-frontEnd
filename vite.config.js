d12c3b173bf30d9eb094a67fc1085a062418fcfc
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-09-01 19:18:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/hotel-management-frontEnd/",
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  },
+  base:"/hotel-management-frontEnd/"
 })
